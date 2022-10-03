486e68093f1ec69ae81fa090fa42e69b3098772a
张坤 <929794551@qq.com>
2022-10-03 19:14:54 +0800

    env

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const config = {
-  title: " 狂奔滴小马 | 前端工程师成长部落阁",
-  description: "分享 JavaScript 以及热门框架，记录前端工程师学习成长历程。",
-  author: "狂奔滴小马",
+  title: " zkrisj | 个人博客",
+  description: "全栈技术文章记录博客。",
+  author: "zkrisj",
   avatar:
-    "https://p3-passport.byteimg.com/img/user-avatar/585e1491713363bc8f67d06c485e8260~200x200.awebp",
-  banner: "Xiaoma Blog",
+    "https://zkrisj.gitee.io/favicon.ico",
+  banner: "zkrisj Blog",
 };
